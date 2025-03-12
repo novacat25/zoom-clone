b6229389083a5ff94b1edf31bcc9ad2ebbbb2a02
NovaCat <sparkstorms2@gmail.com>
2025-03-13 00:51:29 +0900

    Feat: Basic Frontend setup

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ import express from "express"
 const app = express()
 const PORT_NUMBER = 3000
 
-console.log("hello")
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+app.use("/public", express.static(__dirname + "/public"))
+app.get("/", (req,res) => res.render("home"))
 
-app.listen(PORT_NUMBER)
+const handleListen = () => console.log(`Listening on http://localhost:${PORT_NUMBER}`)
+app.listen(PORT_NUMBER, handleListen)
