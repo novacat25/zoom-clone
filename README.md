4e32aa5bf46b5bba669d709984e986e4b75fba2a
NovaCat <sparkstorms2@gmail.com>
2025-03-12 22:50:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Noom
+Zoom Clone using NodeJS, WebRTC and Websockets.
+This course is from [NomadCoders](https://nomadcoders.co/)
---
+++ src/server.js
@@ -0,0 +1,8 @@
+import express from "express"
+
+const app = express()
+const PORT_NUMBER = 3000
+
+console.log("hello")
+
+app.listen(PORT_NUMBER)
