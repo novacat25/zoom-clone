dca57d70723bb3f1535787d2e93519d63cffadd6
NovaCat <sparkstorms2@gmail.com>
2025-03-15 23:03:56 +0900

    Refactor: change req to _(Unused parameter)

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const PORT_NUMBER = 3000
 app.set("view engine", "pug")
 app.set("views", __dirname + "/views")
 app.use("/public", express.static(__dirname + "/public"))
-app.get("/", (req,res) => res.render("home"))
-app.get("/*", (req,res) => res.redirect("/"))
+app.get("/", (_,res) => res.render("home"))
+app.get("/*", (_,res) => res.redirect("/"))
 
 const handleListen = () => console.log(`Listening on http://localhost:${PORT_NUMBER}`)
 app.listen(PORT_NUMBER, handleListen)
