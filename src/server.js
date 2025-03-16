9a814df580d884d492597b04f142e10e3cf82f32
NovaCat <sparkstorms2@gmail.com>
2025-03-16 20:15:50 +0900

    Feat: connecting & communicating between FE and BE

--- src/server.js
+++ src/server.js
@@ -16,5 +16,10 @@ const handleListen = () => console.log(`Listening on http://localhost:${PORT_NUM
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
-wss.on("connection", (socket) => console.log(socket))
+wss.on("connection", (socket) => {
+    console.log("Connected to the Browser ✅")
+    socket.on("close", ()=>console.log("Disconnected from the Broswer 😴"))
+    socket.on("message", (message) => console.log(message.toString("utf8")))
+    socket.send("OiiaOiia", console.error)
+})
 server.listen(PORT_NUMBER, handleListen)
