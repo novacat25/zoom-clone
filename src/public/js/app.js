b70dd884fd4d906d999c4ba2c392e96c25452630
NovaCat <sparkstorms2@gmail.com>
2025-03-16 20:15:50 +0900

    Feat: connecting & communicating between FE and BE

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,11 @@
 const SOCKET_URL = `ws://${window.location.host}`
-
+const TIME_DELAY = 10000
 const socket = new WebSocket(SOCKET_URL)
+
+socket.addEventListener("message", () => console.log("Connected to the Server ✅"))
+socket.addEventListener("message", (message) => console.log(`New Message: ${message.data}`))
+socket.addEventListener("close", () => console.log("Disconnected to the Server 😴"))
+
+setTimeout(()=>{
+    socket.send("Hello from the browser!")
+}, TIME_DELAY)
