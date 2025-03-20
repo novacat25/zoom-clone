1b875e1fb1aaec669dd615fb41e5969f4720eba0
NovaCat <sparkstorms2@gmail.com>
2025-03-20 15:57:13 +0900

    Feat: make code simpler

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,12 +3,14 @@ const socket = io()
 const welcomeSection = document.getElementById("welcome")
 const roomNameForm = document.getElementById("room-name")
 
+const noticeBackendDone = (msg) => {
+    console.log(`The backend says: ${msg}`)
+}
+
 const handleRoomSubmit = (e) => {
     e.preventDefault()
     const roomInput = document.getElementById("room-input")
-    socket.emit("enter-room", { payload: roomInput.value }, ()=>{
-        console.log("Server is Done!")
-    })
+    socket.emit("enter-room", roomInput.value, noticeBackendDone)
     roomInput.value = ""
 }
 
