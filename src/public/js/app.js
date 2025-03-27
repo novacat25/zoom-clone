3c773eeee9b7c560634e4c9f919f364a4a5b54f0
NovaCat <sparkstorms2@gmail.com>
2025-03-27 22:03:21 +0900

    Feat: nickname function(with a code challenge)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,18 +3,11 @@ const socket = io()
 const welcomeSection = document.getElementById("welcome")
 const roomNameForm = document.getElementById("room-name")
 const roomSection = document.getElementById("room")
+const nicknameForm = document.getElementById("nickname-form")
+const DEFAULT_NICKNAME = "Anonymous"
 
-let roomName = ""
-
-const showRoom = () => {
-    welcomeSection.hidden = true
-    roomSection.hidden = false
-    const roomTitle = document.getElementById("room-title")
-    roomTitle.innerText = roomName
-
-    const chatForm = document.getElementById("chat-form")
-    chatForm.addEventListener("submit", handleMessageSubmit)
-}
+let isUserSetNickname = false
+const isAnonymousUser = (userNickname) => (userNickname === DEFAULT_NICKNAME)
 
 const addMessage = (msg) => {
     const dialogLost = document.getElementById("dialog-list")
@@ -23,9 +16,25 @@ const addMessage = (msg) => {
     dialogLost.appendChild(chatItem)
 }
 
+const handleNicknameSubmit = (e) => {
+    e.preventDefault()
+    isUserSetNickname = true
+    const nicknameInput = document.getElementById("nickname-input")
+    const userNicknameDisplay = document.getElementById("username-display")
+    const userNickName = nicknameInput.value
+    socket.emit("choose-nickname", userNickName)
+    nicknameForm.hidden = true
+    userNicknameDisplay.innerText = ` Hello ${userNickName}!`
+    userNicknameDisplay.hidden = false
+    nicknameInput.value = ""
+}
 
 const handleRoomSubmit = (e) => {
     e.preventDefault()
+    if(!isUserSetNickname) {
+        alert("Please choose a nickname!")
+        return
+    }
     const roomInput = document.getElementById("room-input")
     roomName = roomInput.value
     socket.emit("enter-room", roomInput.value, showRoom)
@@ -42,8 +51,24 @@ const handleMessageSubmit = (e) => {
     messageInput.value = ""
 }
 
-socket.on("welcome-everyone", () => addMessage("Someone has joined!"))
-socket.on("left-room", () => addMessage("Someone has left!")) 
+const showRoom = () => {
+    welcomeSection.hidden = true
+    roomSection.hidden = false
+    const roomTitle = document.getElementById("room-title")
+    roomTitle.innerText = `Room: ${roomName}`
+
+    const chatForm = document.getElementById("chat-form")
+
+    chatForm.addEventListener("submit", handleMessageSubmit)
+}
+
+socket.on("welcome-everyone", (user) => {
+    isAnonymousUser(user) ? addMessage(`Someone has joined!`) : addMessage(`${user} has joined!`)
+})
+socket.on("left-room", (user) => {
+    isAnonymousUser(user) ? addMessage(`Someone has left!`) : addMessage(`${user} has left!`)
+}) 
 socket.on("show-message", addMessage)
 
+nicknameForm.addEventListener("submit", handleNicknameSubmit)
 roomNameForm.addEventListener("submit", handleRoomSubmit)
