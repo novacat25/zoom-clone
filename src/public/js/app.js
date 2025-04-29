03b56dbdcbc5bf748e236bec7cbe5de044bf69a0
NovaCat <sparkstorms2@gmail.com>
2025-04-29 21:23:37 +0900

    Feat: camera switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,23 +1,66 @@
 const socket = io()
+
 const myFace = document.getElementById("my-face")
 const muteButton = document.getElementById("mute")
 const cameraButton = document.getElementById("camera")
+const cameraSelect = document.getElementById("camera-select")
+
+const CAMERA_INPUT_TYPE = "videoinput"
 
 let myStream = null
 let [muted, cameraOff] = [false, false]
 
-const getMedia = async () => {
+const getCameras = async () => {
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true
+        const devices = await navigator.mediaDevices.enumerateDevices()
+        const cameras = devices.filter((device) => device.kind === CAMERA_INPUT_TYPE)
+        const currentCamera = myStream.getVideoTracks()[0]
+
+        cameras.forEach((camera) => {
+            const option = document.createElement("option")
+            option.innerText = camera.label
+            option.value = camera.deviceId
+            if(camera.label === currentCamera.label) {
+                option.selected = true
+            }
+            cameraSelect.appendChild(option)
         })
-        myFace.srcObject = myStream
-        console.log(myStream)
 
     } catch (err) {
         console.error(err)
     }
+
+}
+const getMedia = async (deviceId) => {
+    const initialConstraints = {
+        audio: true,
+        video: {
+            facingMode: "user"
+        }
+    }
+    const cameraConstraints = {
+        audio: true,
+        video: {
+            deviceId: deviceId
+        }
+    }
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstraints
+        )
+        myFace.srcObject = myStream
+
+        if(!deviceId) {
+            await getCameras()
+        }
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const handleCameraChange = async () => {
+    await getMedia(cameraSelect.value)
 }
 
 const handleMuteClick = () => {
@@ -52,3 +95,4 @@ getMedia()
 
 muteButton.addEventListener("click", handleMuteClick)
 cameraButton.addEventListener("click", handleCameraClick)
+cameraSelect.addEventListener("input", handleCameraChange)
